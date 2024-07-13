e243be24892e211599e6d2f6658028ef11ca9ba9
kamal sharma <kamalbsr2101@gmail.com>
2024-07-13 21:37:08 +0530

    change folder and file name array_list to singly_linked_list

--- array_and_list/Question.md
+++
@@ -1,9 +0,0 @@
-1. Array and List
-2. Array module
-3. Creating array
-4. Array methods
-5. List
-6. Creating list object
-7. Methods of list
-8. Build in methods
-9. NumPy
---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,15 @@
+class employee_data():
+    def __init__(self, id, name, salary):
+        self.id = id
+        self.name = name
+        self.salary = salary
+
+    def emp_id():
+        return id
+    def emp_name(clr):
+        return clr.name
+    def emp_salary(clr):
+        return clr.salary
+    
+t1 = employee_data(213213, "kamal", 78000)
+print(t1.salary)
