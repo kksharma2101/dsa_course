4746b185c5cf1dd5a93d8bf069af857158e436cf
kamal sharma <kamalbsr2101@gmail.com>
2024-07-17 08:38:51 +0530

    add signly linked list questions

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,15 +1,27 @@
-class employee_data():
-    def __init__(self, id, name, salary):
-        self.id = id
-        self.name = name
-        self.salary = salary
+#  Assignment-3 Singly linked list
+# Define a class Node to describe a node of a Singly Linked List
 
-    def emp_id():
-        return id
-    def emp_name(clr):
-        return clr.name
-    def emp_salary(clr):
-        return clr.salary
-    
-t1 = employee_data(213213, "kamal", 78000)
-print(t1.salary)
+class Node:
+    def __init__(self, item=None, next=None):
+        self.item = item
+        self.next = next
+
+# Define a class SLLL to implement Singly linked list with __init__() method to create and initialise start reference variable
+
+class SLL:
+    def __init__(self, start):
+        self.start = start
+    def is_empty(self):
+        return self.start == None
+    def insert_at_start(self,data):
+        n = Node(data, self.start)
+        self.start = n
+    def insert_at_last(self, data):
+        n = Node(data)
+        if not self.is_empty():
+            temp = self.start
+            while temp.next is not None:
+                temp = temp.next
+                temp.next = n
+        else:
+            self.start = n
