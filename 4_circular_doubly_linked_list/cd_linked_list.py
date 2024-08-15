81a57082620690874de8da9836409f33331de3a8
kamal sharma <kamalbsr2101@gmail.com>
2024-08-15 21:36:50 +0530

    complete circular doubly linked list

--- 4_circular_doubly_linked_list/cd_linked_list.py
+++ 4_circular_doubly_linked_list/cd_linked_list.py
@@ -44,7 +44,24 @@ class Cdll:
         
 # 6. Define a method search() to find the node with specified element value.
     def search(self, data):
-        pass
+        if self.start == None:
+            pass
+        if not self.is_empty():
+            temp = self.start
+            while temp.next != self.start:
+                if temp.item == data:
+                    return temp
+                temp = temp.next
+            if temp.item == data:
+                return temp
+        return None
+    
+# 7. Define a method insert_after() to insert a new node after a given node of the list.
+    def insert_item(self, temp, data):
+        n = Node(temp, data, temp.next)
+        if temp != None:
+            temp.next = n
+            temp.next.pre = n
 
 # 8. Define a method to print_all() the elements of the list.
     def print_list(self):
@@ -55,9 +72,84 @@ class Cdll:
                 temp = temp.next
             print(temp.item)
 
+# 9. Define a method delete_first() to delete first element from the list.
+    def delete_first(self):
+        if self.start == None:
+            pass
+        if self.start.next == self.start:
+            self.start = None
+        else:
+            self.start.pre.next = self.start.next
+            self.start.next.pre = self.start.pre
+            self.start = self.start.next
+
+# 10. Define a method delete_last() to delete last element from the list.
+    def delete_last(self):
+        if self.start == None:
+            pass
+        if self.start.next == self.start:
+            self.start = None
+        else:
+            self.start.pre.pre.next = self.start
+            self.start.pre = self.start.pre.pre
+            
+# 10. Define a method delete_item() to specified element from the list.
+    def delete_item(self, data):
+        if not self.is_empty():
+            if self.start.next == self.start:
+                if self.start.item == data:
+                    self.start = None
+            else:
+                if self.start.item == data:
+                    self.delete_first()
+                else:
+                    temp = self.start
+                    while temp.next != self.start:
+                        if temp == self.start.pre:
+                            if temp.item == data:
+                                self.delete_last()
+                                break
+                        if temp.item == data:
+                            temp.next.pre = temp.pre
+                            temp.pre.next = temp.next
+                            break
+                        temp = temp.next
+           
+                        
+    def __iter__(self):
+        return CdllIterator(self.start)
+
+class CdllIterator:
+    def __init__(self, start):
+        self.current = start
+        self.count = 0
+        self.start = start
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.current is None:
+            raise StopIteration
+        if self.current == self.start and self.count == 1:
+            raise StopIteration
+        else:
+            self.count = 1
+        data = self.current.item
+        self.current = self.current.next
+        return data
+
 cdll_obj = Cdll()
 cdll_obj.insert_at_start(43)
 cdll_obj.insert_at_start(76)
 cdll_obj.insert_at_last(21)
 cdll_obj.insert_at_last(35)
+cdll_obj.insert_at_last(350)
+cdll_obj.insert_item(cdll_obj.search(21), 89)
+# cdll_obj.delete_first()
+# cdll_obj.delete_last()
+cdll_obj.delete_item(350)
 cdll_obj.print_list()
+for items in cdll_obj:
+    # print(items, end=' ')
+    pass
